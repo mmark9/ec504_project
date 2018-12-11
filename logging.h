3052dce5532d5884d112f32e39cdbc6b143bd05d
Miguel Mark <mmark9@bu.edu>
2018-12-11 10:57:15 -0500

    Add logging.h

---
+++ logging.h
@@ -0,0 +1,15 @@
+/*
+ * logging.h
+ *
+ *  Created on: Dec 10, 2018
+ *      Author: miguel
+ */
+
+#ifndef LOGGING_H_
+#define LOGGING_H_
+
+const bool VERBOSE = false;
+
+
+
+#endif /* LOGGING_H_ */
